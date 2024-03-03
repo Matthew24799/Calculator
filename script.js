ff6776f658a7e458abb8959de57cb90b4242c489
Matthew24799 <157596330+Matthew24799@users.noreply.github.com>
2024-03-03 18:56:32 -0500

    added math logic
    created basic math operations that work with prompts and sends the results into the console

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 
+let num1 = parseInt(prompt("Enter first number"));
+let Operator = prompt("Enter Operator, +,-,*, or /");
+let num2 = parseInt(prompt("Enter second number"));
 
+if(!isNaN(Operator)) {
+    Operator = prompt("MUST BE + , - , * OR / ");
+};
+
+
+function comeput() {
+    let computation
+    const firstNum = num1;
+    const currentNum = num2;
+
+    switch(Operator) {
+     case "+": computation = firstNum + currentNum;
+     break;
+     case "-": computation = firstNum - currentNum;
+     break;
+     case "*": computation = firstNum * currentNum;
+     break;
+     case "/": computation = firstNum / currentNum;
+     break;
+    };
+        return computation;
+};
+
+console.log("Num1: " + num1);
+console.log("operator: " + Operator);
+console.log("num2: " + num2);
+
+console.log("Answer is: " + comeput());
 
